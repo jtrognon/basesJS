189e11fe645ae6848fc1b5881eee739ff182303b
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-07-14 15:27:43 +0200

    Update index.js

--- quiz/index.js
+++ quiz/index.js
@@ -10,7 +10,8 @@ baliseHTML.style.background = "yellow";
 const questionContainer = document.querySelector(".click-event");
 const btn1 = document.querySelector("#btn-1");
 const btn2 = document.getElementById("btn-2");
-const response = document.querySelector("p");
+const response = document.querySelector('p');
+
 
 questionContainer.addEventListener("click", () => {
   questionContainer.classList.toggle("question-clicked");
