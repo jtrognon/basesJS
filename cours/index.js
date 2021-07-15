1e34f9b516603419f285a961298390e5d157e978
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-07-15 13:22:21 +0200

    Scroll event

--- cours/index.js
+++ cours/index.js
@@ -90,3 +90,19 @@ document.addEventListener("keypress", (e) => {
 
   ring();
 });
+
+//--------------------------------------------------------------
+//Scroll event
+
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 120){
+    nav.style.top = 0;
+  }
+  else {
+    nav.style.top = "-50px";
+  }
+})
+
+//--------------------------------------------------------------
