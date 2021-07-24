5bc202bfe2ead75bf453ca46bd6fe22b99c42438
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-07-24 16:55:44 +0200

    structures de controle

--- P3/cours/index.js
+++ P3/cours/index.js
@@ -37,10 +37,60 @@ let data = [
   },
   {
     pseudo: "Nicolas",
-    age: 33,
+    age: 42,
     technos: ["PHP", "Python", "Java"],
     admin: true,
   },
 ];
 
 console.log(data[2].pseudo);
+
+//---------------------------------------
+//Les structures de controle
+//---------------------------------------
+if (data[0].age > data[1].age) {
+  // console.log(data[0].pseudo + " est plus agé(e) que " + data[1].pseudo);
+} else {
+  //Valeur si faux
+}
+
+//While
+let w = 0;
+
+while (w < 10) {
+  w++;
+  console.log(w);
+}
+
+let d = 0;
+
+do {
+  d++;
+  console.log(d);
+} while (d < 5);
+
+//Les boucles for
+// for (const user of data) {
+//   document.body.innerHTML += `<li>${user.pseudo} - ${user.age}ans<\li>`;
+// }
+
+for (i = 0; i < data.length; i++) {
+  user = data[i];
+  document.body.innerHTML += `<h2>${user.pseudo} - ${user.age}ans<\h2>`;
+}
+
+//Switch
+document.body.addEventListener("click", (e) => {
+  switch (e.target.id) {
+    case "Javascript":
+      document.body.style.background = "yellow";
+      break;
+    case "Python":
+      document.body.style.background = "blue";
+      break;
+    case "Java":
+      document.body.style.background = "red";
+    default:
+      null;
+  }
+});
