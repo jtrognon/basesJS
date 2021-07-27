92dae30f736b09ab257c618a498d2204e4bc122e
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-07-27 13:24:51 +0200

    methods String + beginning numbers

--- P3/cours/index.js
+++ P3/cours/index.js
@@ -43,7 +43,7 @@ let data = [
   },
 ];
 
-console.log(data[2].pseudo);
+// console.log(data[2].pseudo);
 
 //---------------------------------------
 //Les structures de controle
@@ -55,19 +55,19 @@ if (data[0].age > data[1].age) {
 }
 
 //While
-let w = 0;
+// let w = 0;
 
-while (w < 10) {
-  w++;
-  console.log(w);
-}
+// while (w < 10) {
+//   w++;
+//   console.log(w);
+// }
 
-let d = 0;
+// let d = 0;
 
-do {
-  d++;
-  console.log(d);
-} while (d < 5);
+// do {
+//   d++;
+//   console.log(d);
+// } while (d < 5);
 
 //Les boucles for
 // for (const user of data) {
@@ -94,3 +94,42 @@ document.body.addEventListener("click", (e) => {
       null;
   }
 });
+
+//----------------
+// Méthodes String
+//----------------
+
+let string2 = "Javascript est un langage orienté objet";
+
+// console.log(typeof string2);
+// console.log(eval(parseInt("1") + 2));
+// console.log(isNaN(string2));
+
+// console.log(string2.length);
+// console.log(string2[string2.length - 1]);
+
+// console.log(string2.indexOf("langage"));
+// console.log(string2.indexOf("x")); //retourne -1 s'il ne connait pas
+
+//let newString = string2.slice(2);
+// let newString = string2.slice(5, 17);
+// console.log(newString);
+
+// console.log(string2.split(" "));
+
+// console.log(string2.toLowerCase());
+// console.log(string2.toUpperCase());
+
+// console.log(string2.replace("Javascript", "PHP"));
+
+//--------------------------
+// Méthodes pour les numbers
+//--------------------------
+
+let number2 = 42.1234;
+let numberString = "42.12 est un chiffre";
+
+console.log(number2.toFixed(2));
+console.log(parseInt("43"));
+console.log(parseInt(numberString));
+console.log(parseFloat(numberString));
