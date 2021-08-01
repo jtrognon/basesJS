f2caf91ed9ad636fc3244a19f3ba44055aa1e6ef
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-08-01 14:52:49 +0200

    beginning P4

---
+++ P4/Cours/index.js
@@ -0,0 +1,11 @@
+//XMLHttpRequest
+
+function reqListener() {
+  console.log(this.responseText);
+}
+
+let req = new XMLHttpRequest();
+req.onload = reqListener;
+// req.open("get", "data.txt", true);
+req.open("get", "data.json", true);
+req.send();
