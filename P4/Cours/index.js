5067149eed96e12030130b4ea3316a394570184e
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-08-01 14:54:59 +0200

    XMLHttpRequest

--- P4/Cours/index.js
+++ P4/Cours/index.js
@@ -7,5 +7,6 @@ function reqListener() {
 let req = new XMLHttpRequest();
 req.onload = reqListener;
 // req.open("get", "data.txt", true);
-req.open("get", "data.json", true);
+// req.open("get", "data.json", true);
+req.open("get", "https://api.blablagues.net/?rub=blagues", true);
 req.send();
