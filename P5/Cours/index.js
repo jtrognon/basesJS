7cbf58e9d5e1bf09bb8c7243e7652a92ae6b9a75
jtrognon <74874192+jtrognon@users.noreply.github.com>
2021-08-04 13:14:55 +0200

    Methods objects

--- P5/Cours/index.js
+++ P5/Cours/index.js
@@ -2,6 +2,7 @@ const obj = {
   //index: value
   pseudo: "From Scratch",
   ville: "Bordeaux",
+  admin: true,
 
   direBonjour: function () {
     console.log("Bonjour je suis " + this.pseudo);
@@ -26,3 +27,33 @@ delete obj.ville;
 //Checker si propriété existe
 // console.log("pseudo" in obj);
 // console.log("ville" in obj);
+
+//Obtenir les clés
+const keys = Object.keys(obj);
+// console.log(keys);
+
+//Obtenir les valeurs
+const values = Object.values(obj);
+// console.log(values);
+
+const nestedArray = Object.entries(obj);
+// console.log(nestedArray);
+
+const obj2 = {
+  taille: "1m80",
+  poids: "75Kg",
+  pseudo: "From",
+};
+
+//Fusionner objets
+const fusion = Object.assign({}, obj, obj2);
+// console.log(fusion);
+
+//Empecher les modifications
+// const newObj = Object.freeze(obj);
+const newObj = Object.seal(obj);
+
+newObj.pseudo = "Test";
+newObj.adresse = "42 avenue du code";
+
+// console.log(newObj);
